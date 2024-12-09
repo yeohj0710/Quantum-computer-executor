7782ffb856cdc8a0ff29535c69752e5b0be982cb
yeohj0710 <yeohj0710@yonsei.ac.kr>
2024-12-09 11:40:34 +0900

    2412091140

--- quantum_local.py
+++ quantum_local.py
@@ -1,14 +1,13 @@
 from qiskit import QuantumCircuit, transpile
 from qiskit_aer import Aer
 
+simulator = Aer.get_backend("qasm_simulator")
 qc = QuantumCircuit(1, 1)
+
 qc.h(0)
 qc.measure(0, 0)
 
-simulator = Aer.get_backend("qasm_simulator")
-
 compiled_qc = transpile(qc, simulator)
-
 job = simulator.run(compiled_qc, shots=1000)
 result = job.result()
 counts = result.get_counts(qc)
